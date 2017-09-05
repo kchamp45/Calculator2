34194c320973ca7093f0935f1fd6739afbb85cb6
Guest User <Guest@Epicodus-6E.local>
2017-09-05 16:38:19 -0700

    add testing via karma

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -1,23 +1,21 @@
 var Calculator = require('./../js/pingpong.js').calculatorModule;
 
 describe('Calculator', function(){
+  var simpleCalculator;
+
+  beforeEach(function() {
+    simpleCalculator = new Calculator("hot pink");
+  });
 
   it('should test whether it can instantiate', function() {
-    var simpleCalculator = new Calculator("hot pink")
     expect(simpleCalculator.skin).toEqual("hot pink")
   });
-});
 
-describe('Calculator', function() {
   it ('should test whether it can add two numbers', function() {
-    var simpleCalculator = new Calculator("hot pink")
     expect(simpleCalculator.basicCalc(2, 3, "add")).toEqual(5);
   });
-});
-
-describe('Calculator', function() {
+  
   it ('should test whether it returns "ping" when divisible by 3', function() {
-    var simpleCalculator = new Calculator("hot pink")
     expect(simpleCalculator.pingPong(3)).toEqual([1, 2, "ping"]);
   });
 });
